c8b3b0c5a30e322b1ca4a3f81f949970b2812b34
roza <rozalizadeh@gmail.com>
2023-07-24 12:50:46 +0330

    second-exercise-اضافات دوم

---
+++ second-exercise/index.js
@@ -0,0 +1,23 @@
+
+let strs=''
+
+for(let i=0 ; i<8 ; i+=1){
+  for(let j=0 ; j<8 ;j+=1){
+    if((i+j)%2==0){
+      
+      
+      strs+= ' ';
+      
+    }
+    else {
+      strs+=  '#';
+      
+         }
+                            }
+       strs+="\n";
+
+     
+     
+
+}
+console.log(strs)
